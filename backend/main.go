6ea9832039e0b62f7b6422d0e996d33e5f1be3c1
Gabriel Rodrigues <gabriel.rodrigues.brito@gmail.com>
2023-10-10 17:59:38 -0300

    rota de listagem com echo

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"net/http"
+
 	_ "github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	// conexão com o banco
 	db, err := ConnectDatabase()
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
+
+	// inicializando o echo
+	e := echo.New()
+
+	// rotas
+	e.GET("/coins", func(c echo.Context) error {
+		var coins []Coin
+
+		if err := db.Find(&coins).Error; err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Erro ao buscar os registros"})
+		}
+
+		return c.JSON(http.StatusOK, coins)
+	})
+
+	e.Start(":8080")
 }
